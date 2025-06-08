dd886c67e2e6955a47a9583a8c9103a094c16382
Andrew Abramov <andrabra@yandex.ru>
2025-06-08 20:20:31 +0500

    Починил ошÐПоправил остальные ошикбки для деплоя

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { lazy, Suspense, useState } from 'react';
-import { Alert } from 'antd';
+import { lazy, Suspense,  } from 'react';
 import Home from './pages/Home';
 import Loader from './components/Loader';
 import * as React from 'react';
@@ -11,35 +10,29 @@ const Recipes = lazy(() => import('./pages/Recipes'));
 
 // Кастомный ErrorBoundary с использованием Ant Design
 const AppErrorBoundary = ({ children }: { children: React.ReactNode }) => {
-  const [hasError, setHasError] = useState(false);
+  // const [hasError, setHasError] = useState(false);
 
   // const handleOnError = (error: Error, info: { componentStack: string }) => {
   //   console.error('Error caught by ErrorBoundary:', error, info);
   //   setHasError(true);
   // };
 
-  if (hasError) {
-    return (
-      <Alert
-        message="Произошла ошибка"
-        description="Попробуйте перезагрузить страницу или вернуться позже"
-        type="error"
-        showIcon
-        style={{ margin: 24 }}
-      />
-    );
-  }
+  // if (hasError) {
+  //   return (
+  //     <Alert
+  //       message="Произошла ошибка"
+  //       description="Попробуйте перезагрузить страницу или вернуться позже"
+  //       type="error"
+  //       showIcon
+  //       style={{ margin: 24 }}
+  //     />
+  //   );
+  // }
 
   return children;
 };
 
 const App = () => {
-  // Функция для предзагрузки компонентов
-  const preloadRecipes = () => {
-    import('./pages/Recipes').catch(error => {
-      console.error('Ошибка предзагрузки:', error);
-    });
-  };
 
   return (
     <BrowserRouter>
