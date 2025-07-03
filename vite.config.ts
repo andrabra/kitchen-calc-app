1f969019937479c51a1341f76e31017010eb944a
Andrew Abramov <andrabra@yandex.ru>
2025-07-03 22:14:51 +0500

    Добавил react compiler rc

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => {
   return {
     base: command === 'serve' ? '/' : '/kitchen-calc-app/',
-    plugins: [react()],
+    plugins: [
+      react({
+        babel: {
+          plugins: ['babel-plugin-react-compiler'],
+        },
+      }),
+    ],
   };
 });
